2b5ffa4d10898718f4aa3a2d4cf1fa5d8b2b6cdb
是柠新呀 <892573319@qq.com>
2021-05-25 11:20:02 +0800

    完成了页面初始化以及tab组件

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <m-header></m-header>
+  <tab></tab>
+  <router-view></router-view>
+</template>
+
+<script>
+import Header from '@/components/header/header'
+import Tab from '@/components/tab/tab'
+export default {
+  name: 'app',
+  components: {
+    MHeader: Header,
+    Tab
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
